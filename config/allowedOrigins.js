5380c1dd147339aadaf799101c48aab39091a5ce
Neelkanth <61936264+NeelkanthSingh@users.noreply.github.com>
2024-04-29 14:55:28 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
+    'https://versionvaulthub.com',
     'https://www.versionvaulthub.com',
-    'http://127.0.0.1:5173',
-    'http://localhost:5173',
     'https://vvh-prod-frontend-45twlzui1-neelkanths-projects.vercel.app'
 ];
 
