7d1ab466e3a47c6d8028f0bedb8108dbd89eb96d
Neelkanth <61936264+NeelkanthSingh@users.noreply.github.com>
2024-04-29 15:21:03 +0530

    Update authController.js secure to true

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,7 @@ authController.checkUser = async (req, res) => {
 
 authController.logoutUser = async (req, res) => {
     console.log("Logging out user");
-    res.clearCookie('jwtToken', { httpOnly: true, sameSite: 'lax', secure: false});
+    res.clearCookie('jwtToken', { httpOnly: true, sameSite: 'lax', secure: true});
     return res.sendStatus(204);
 };
 
