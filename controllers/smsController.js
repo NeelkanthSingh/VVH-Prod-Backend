3ced9ca1c3f51b4c349fd710f9c2f6b46699470b
NeelkanthSingh <neelkanthsingh.jr@gmail.com>
2024-05-15 00:27:10 +0530

    "Different Project Testing VIII"

--- controllers/smsController.js
+++ controllers/smsController.js
@@ -2,6 +2,9 @@ const smsController = {};
 
 smsController.readMssgs = (req, res) => {
     console.log("Reading messages");
+    const mssg = req.body.sms_message;
+    const phone = req.body.sender_phone_number;
+
     console.log("Message: ", mssg);
     console.log("Phone: ", phone);
     res.json({
